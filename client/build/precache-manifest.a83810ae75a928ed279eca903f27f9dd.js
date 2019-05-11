f007a71fc6a5773fcfee8879eb377d728fbdbf13
amirrkhan <aliamirovaxe@gmail.com>
2019-05-11 16:01:46 +0200

    build added

---
+++ client/build/precache-manifest.a83810ae75a928ed279eca903f27f9dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "7782c1e4f1e62d4da1d9",
+    "url": "/static/js/main.7782c1e4.chunk.js"
+  },
+  {
+    "revision": "8ee41f20290b30a34eae",
+    "url": "/static/js/2.8ee41f20.chunk.js"
+  },
+  {
+    "revision": "7782c1e4f1e62d4da1d9",
+    "url": "/static/css/main.7fa63eb5.chunk.css"
+  },
+  {
+    "revision": "84a6d8a19a23912510151df6f698f094",
+    "url": "/index.html"
+  }
+];
